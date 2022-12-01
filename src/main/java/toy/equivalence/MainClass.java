e78fa1f276812773d63d50883e51718ae2f83e86
Cloud Creator <qingyun_yang@foxmail.com>
2022-12-01 11:36:20 +0800

    add Main Class

---
+++ src/main/java/toy/equivalence/MainClass.java
@@ -0,0 +1,9 @@
+package toy.equivalence;
+
+import toy.equivalence.verify.MainApplication;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MainApplication.main(args);
+    }
+}
